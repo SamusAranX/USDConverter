4bf5ccf052ca55eb05b3834aaa9bc3228d323aec
Emma Alyx Wunder <hallo@emmalyx.dev>
2019-06-11 12:58:28 +0200

    Initial Commit

---
+++ USDConverter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  USDConverter
+//
+//  Created by Emma Alyx Wunder on 11.06.19.
+//  Copyright © 2019 Emma Alyx Wunder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
