a836e9ac05d7e26983859454019f34918b3cf146
Emma Alyx Wunder <hallo@emmalyx.dev>
2019-12-10 16:54:32 +0100

    Added preliminary testing code to parse .mtl files or analyze .obj materials directly

---
+++ USDConverter/MTLParser/MTLLineType.swift
@@ -0,0 +1,40 @@
+//
+//  MTLLineType.swift
+//  USDConverter
+//
+//  Created by Emma Alyx Wunder on 10.12.19.
+//  Copyright © 2019 Emma Alyx Wunder. All rights reserved.
+//
+
+import Cocoa
+
+enum MTLLineType: String {
+
+    case newmtl
+
+//    case Kd
+//    case Ka
+//    case Ks
+//
+//	case d
+//	case ao
+//	case subsurface
+//	case metallic
+//	case specularTint
+//	case roughness
+//	case anisotropicRotation
+//	case sheen
+//	case sheenTint
+//	case clearCoat
+//	case clearCoatGloss
+//
+//	case map_Kd
+//	case map_Ka
+//	case map_d
+//	case map_ao
+//
+//	case map_metallic
+//	case map_roughness
+//	case map_tangentSpaceNormal
+
+}
---
+++ USDConverter/MTLParser/MTLParser.swift
@@ -0,0 +1,13 @@
+//
+//  MTLParser.swift
+//  USDConverter
+//
+//  Created by Emma Alyx Wunder on 10.12.19.
+//  Copyright © 2019 Emma Alyx Wunder. All rights reserved.
+//
+
+import Cocoa
+
+class MTLParser {
+	// TODO
+}
